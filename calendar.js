4779af9bf6eafe4d5bf9463d959fd91a9c53be70
Alex Brovkin <alex.brovkin@shopify.com>
2018-11-21 21:24:51 -0500

    shifted to right and cmyk colors

--- calendar.js
+++ calendar.js
@@ -4,12 +4,13 @@
 //- [ ] add CMYK colours, e.g. fill="#CD853F device-cmyk(0.11, 0.48, 0.83, 0.00)", 
 //      in SVGtoPDF there is an option colorCallback [function] = function called to get color, making mapping to CMYK possible
 //- [ ] make line adjustment dynamic or try baseline: anchor: Anchor of object in coordinate. (default: 'left baseline') ... (left, center, right) + (baseline, top, middle, bottom)
-//- [ ] set colours: grey,pink,lightgrey,black,very light grey (#eeeeee)
+//- [x] set colours: ...,,,,,
 //- [ ] need to argument passed to tracker to take into account weekStartsOn
 //- [ ] top crop lines don't align
 // settings:
 cal_w = 5.5*72
 page_hPadding = 3/16*72
+page_hShift = 1/16*72
 page_w = cal_w-2*page_hPadding
 page_h = 3.375*72
 bleed = 1/8*72
@@ -44,10 +45,10 @@ var doc = new PDFDocument({
 
 // outlining
 const TextToSVG = require('text-to-svg');
-const textToSVG = TextToSVG.loadSync('fonts/nimbus mono d7dfb1f6-0918-41e9-a9d2-bf7241c11fae.ttf');
+const textToSVG = TextToSVG.loadSync('fonts/nimbus bold 724726a7-b3d6-4c01-ac68-73ef3673e3e1.ttf');
 const monthToSVG = TextToSVG.loadSync('fonts/nimbus 1 - dc9d32c4-6c53-4bb1-8bef-4cd396bee3ce.ttf');
 const digitsToSVG = TextToSVG.loadSync('fonts/nimbus mono d7dfb1f6-0918-41e9-a9d2-bf7241c11fae.ttf');
-const weeksToSVG = TextToSVG.loadSync('fonts/nimbus italic 940b0050-c3b7-41ea-af4b-857bc9e8313d.ttf');
+const weeksToSVG = TextToSVG.loadSync('fonts/nimbus regular a70c1179-4a1d-4887-8eb1-d4f6ce17cfb4.ttf');
 
 // INPUTS
 var start = {year: 2018, month: 12} // 1 = january
@@ -109,6 +110,7 @@ var dataSVG = (page_i,calendar=calPages) => {
         // console.log(adjustment)
         var day = group.path(digitsToSVG.getD(d, options))
                     .move(x+padding_x, y+padding_y)//y-adjustment.height+18+padding_y)
+                    .fill('aqua')
     }
     var writeDays = (days,m,n,wks) => {
         h = page_h / wks
@@ -117,17 +119,17 @@ var dataSVG = (page_i,calendar=calPages) => {
         })
     }
     var writeDaysOfWeek = (m) => {
-        options = {anchor: 'right baseline', fontSize: 6}
+        options = {anchor: 'right baseline', fontSize: 5}
         daysOfWeek.forEach((d,j)=>{
             adjustment=textToSVG.getMetrics(d.toUpperCase(), options)
             var wDay = group.path(textToSVG.getD(d.toUpperCase(), options))
                         .move((j+1)*page_w/7-padding_x-adjustment.width+2, m*page_h+padding_y)//m*page_h-adjustment.height+18+padding_y)
-                        .fill('grey')
+                        .fill('lightgrey')
         })
     }
     var writeMonthHeader = (m,mmmm,wks) => {
         h = page_h / wks
-        options = {fontSize: 24}
+        options = {fontSize: 25}
         adjustment=monthToSVG.getMetrics(mmmm, options)
         var month = group.path(monthToSVG.getD(mmmm, options))
                     .move(padding_x, m*page_h+h-adjustment.ascender+2)
@@ -135,7 +137,7 @@ var dataSVG = (page_i,calendar=calPages) => {
     }
     var writeWeekNumber = (w,m,n,wks) => {
         h = page_h / wks
-        options = {fontSize: 6}
+        options = {fontSize: 5}
         adjustment=weeksToSVG.getMetrics((n==1?"Week ":"") + w, options)
         var week = group.path(weeksToSVG.getD((n==1?"Week ":"") + w, options))
             .move(padding_x, m*page_h+(n+1)*h-adjustment.ascender+adjustment.descender)
@@ -159,7 +161,7 @@ var dataSVG = (page_i,calendar=calPages) => {
         var h = group
                 .rect(2*page_w/7+page_hPadding+bleed, page_h*number_of_months+2*bleed)
                 .move(5*page_w/7,-bleed)
-                .fill('#eeeeee')
+                .fill('aliceblue')
     }
     var drawCropMarks = () => {
         var cropMark = draw.symbol()
@@ -180,12 +182,13 @@ var dataSVG = (page_i,calendar=calPages) => {
         //     x: -1, 
         //     y: 0
         // }).fill('red')
+        
+        // 1/2 is adjustment for thickness of crop line to center it
+        var topLeft = margins.use(cropMark).move(0, 1/2)
+        var topRight = margins.use(cropMark).rotate(90,0,0).move(1/2, -cal_w)
 
-        var topLeft = margins.use(cropMark).move(0, 0)
-        var topRight = margins.use(cropMark).rotate(90,0,0).move(0, -cal_w)
-
-        var bottomRight = margins.use(cropMark).rotate(180,0,0).move(-cal_w, -maxPages*page_h)
-        var bottomLeft = margins.use(cropMark).rotate(270,0,0).move(-maxPages*page_h, 0)
+        var bottomRight = margins.use(cropMark).rotate(180,0,0).move(-cal_w, -maxPages*page_h+1/2)
+        var bottomLeft = margins.use(cropMark).rotate(270,0,0).move(-maxPages*page_h-1/2, 0)
 
         var lip1 = margins.rect(36, 1).attr({ 
             x: -42, 
@@ -206,17 +209,17 @@ var dataSVG = (page_i,calendar=calPages) => {
         if(p==0) {
             var rect0 = tracker.rect(width+bleed, top-page_h/wks1/7+bleed)
             .move(-bleed,-bleed)    
-            .fill('#eeeeee')
+            .fill('aliceblue')
         }
 
         var rect = tracker.rect(width+bleed, height)
             .move(-bleed,m*page_h+top)    
-            .fill('#eeeeee')
+            .fill('aliceblue')
     }
 
     // SET UP FILE  
     main.move(cal.paper_margin*72,cal.paper_margin*72)
-    group.move(page_hPadding,0)
+    group.move(page_hPadding+page_hShift,0)
     drawCropMarks()
     drawWeekend()
     for(i=1;i<7;i++) drawDayLine(i)
@@ -245,7 +248,7 @@ var dataSVG = (page_i,calendar=calPages) => {
     // tracker.clipWith(clip)
 
     //moving for next pages
-    group.move(page_hPadding,-page_i*maxPages*page_h)
+    group.move(page_hPadding+page_hShift,-page_i*maxPages*page_h)
     clipRect.move(-bleed,page_i*maxPages*page_h-bleed)
 
     // get your svg as string
@@ -253,13 +256,13 @@ var dataSVG = (page_i,calendar=calPages) => {
     // or
     // console.log(draw.node.outerHTML)
 
-    // for debug
-    fs.writeFile("test"+page_i+".svg", draw.node.outerHTML, function(err) {
-        if(err) {
-            return console.log(err);
-        }    
-        console.log("Also made you a svg <3");
-    }); 
+    // // for debug
+    // fs.writeFile("test"+page_i+".svg", draw.node.outerHTML, function(err) {
+    //     if(err) {
+    //         return console.log(err);
+    //     }    
+    //     console.log("Also made you a svg <3");
+    // }); 
 
     var cleanSVG = (data) => {
         var returndata = data.replace(/(<svg.*?>)<svg.*?>(.*?)<\/svg>/g, '$1')
@@ -280,7 +283,48 @@ for(var page_i=0; page_i < Math.ceil(number_of_months/maxPages); page_i++) {
     // if(page_i>0) doc.addPage()//.addSVG(cleanSVG, 0*cal.paper_margin*72, 0*cal.paper_margin*72);
     console.log("Page "+(page_i+1)+" of "+Math.ceil(number_of_months/maxPages))
     doc.addPage()
-    doc.addSVG(dataSVG(page_i), 0*cal.paper_margin*72, 0*cal.paper_margin*72, {assumePt: true});
+    doc.addSVG(
+        dataSVG(page_i), 
+        0*cal.paper_margin*72, 
+        0*cal.paper_margin*72, 
+        {
+            assumePt: true,
+            colorCallback: function(color) {
+                // pink
+                if( color[0][0]==255 && color[0][1]==192 && color[0][2]==203 ) {
+                    return [[0,100,0,0],1]
+                    return color
+                }
+                // black
+                else if( color[0][0]==0 && color[0][1]==0 && color[0][2]==0 ) {
+                    return [[0,0,0,100],1]
+                    return color
+                } 
+                // grey
+                else if( color[0][0]==128 && color[0][1]==128 && color[0][2]==128 ) {
+                    return [[0,0,0,50],1]
+                    return color
+                } 
+                // lightgrey
+                else if( color[0][0]==211 && color[0][1]==211 && color[0][2]==211 ) {
+                    return [[0,0,0,25],1]
+                    return color
+                } 
+                // fake alice blue 240,248,255 for #eeeee very light grey 
+                else if( color[0][0]==240 && color[0][1]==248 && color[0][2]==255 ) {
+                    return [[0,0,0,10],1]
+                    return color
+                } 
+                // fake aqua 0,255,255 for charcoal 
+                else if( color[0][0]==0 && color[0][1]==255 && color[0][2]==255 ) {
+                    return [[0,0,0,75],1]
+                    return color
+                } 
+                else {
+                    return color
+                }
+            }
+        });
 }
 
 stream.on('finish', function() {
