b77a148618eceb0dd1513b126b2af3504306fe99
Alex Brovkin <alex@webmovement.com>
2023-02-01 23:55:24 -0500

    more refactor

--- index structure.js
+++ index structure.js
@@ -1,11 +1,14 @@
 // structure attempt
 
+// TODO
+// - [ ] Next: tackle Designer function next
+
 //each probably in separate file
 // ⚠️⚠️⚠️
 // multiple page split makes things even more complicated here
 // e.g. generate one print page at a time
 // ⚠️⚠️⚠️
-Mechanical {
+function Mechanical() {
     // add crop marks, split to printable pages
     // ....
     var maxPages = Math.floor((cal.paper_h-cal.paper_margin*2)*72/page_h)
@@ -28,7 +31,7 @@ Mechanical {
         }
     }
 }
-SVGRenderer {
+function SVGRenderer() {
     //... generate SVG
     // returns a window with a document and an svg root node
     const window   = require('svgdom')
@@ -43,7 +46,7 @@ SVGRenderer {
     //to use
     Designer
 }
-Designer {
+function Designer() {
     // all the spacing, settings, etc
     // from
     var templates = {
@@ -56,7 +59,8 @@ Designer {
             height: 3.5-1/8
         }
     }
-    var showMonthTracker = (weekStartsOn == 1) ? true : false
+    var showMonthTracker = (week_starts_on == 1) ? true : false
+
     cal_w = templates[template].width*72
     page_hPadding = 3/16*72
     page_hShift = 1/16*72
@@ -70,49 +74,54 @@ Designer {
         cal.paper_w = templates[template].paper_w
         cal.paper_h = (templates[template].paper_h=="roll") ? page_h/72*number_of_months+2*cal.paper_margin : templates[template].paper_h
 
-    Paginator...
+    // Paginator...
 }
-function Paginator(calWeeks = myCalendar.calWeeks) {
-    // split by calendar pages from the raw data
-    // from
-    var calPages = ((weeks) => {
-        calPages = [[]]
-        var i = 0, j=0
-        while (i < weeks.length) {
-            // if day0 > day6 (e.g. 28 > 4) then it's last week of month which is carried over to next month
-            isLastWeek = dateFns.getDate(weeks[i][0]) > dateFns.getDate(weeks[i][6])
-            // if day0 is on 1st OR if last week but not first or last week then next page
-            if( (j!=0?dateFns.isFirstDayOfMonth(weeks[i][0]):false) || (isLastWeek && i!=0 && i!=weeks.length-1)) {
-                j+=1
-                calPages[j]=[]
-            }
-            calPages[j].push(weeks[i])
-            i++
-        }
-        return calPages
-    })(calWeeks)
-}
-function Calendar(start, number_of_months, weekStartsOn) {
+
+class CalendarData {
     // raw calendar _data_
     // ... dateFns
-    const dateFns = require('date-fns');
-    var start_date = new Date(start.year, start.month-1, 1)
-    var end_date = dateFns.lastDayOfMonth(dateFns.addMonths(start_date,number_of_months-1))
-    //static
-    var daysOfWeek = [...Array(7).keys()].map((d)=>{return dateFns.format(dateFns.addDays(dateFns.startOfWeek(start_date,{weekStartsOn:weekStartsOn}),d),'ddd')})
-    // CREATE CALENDAR DATA
-    var calStart = dateFns.startOfWeek(start_date,{weekStartsOn:weekStartsOn})
-    var calEnd   = dateFns.endOfWeek(end_date,{weekStartsOn:weekStartsOn})
-    var calDays = dateFns.eachDay(calStart, calEnd)
-    var calWeeks = ((days) => {
-	calWeeks = []
-	while (days.length) {
-		calWeeks.push(days.splice(0, 7));
-	}
-	return calWeeks
-})(calDays)
+    constructor(settings) {
+        // private date crunching
+        const dateFns = require('date-fns');
+        var start_date = new Date(settings.start.year, settings.start.month-1, 1)
+        var end_date = dateFns.lastDayOfMonth(dateFns.addMonths(start_date,settings.number_of_months-1))
+        var calStart = dateFns.startOfWeek(start_date,{weekStartsOn:settings.week_starts_on})
+        var calEnd = dateFns.endOfWeek(end_date,{weekStartsOn:settings.week_starts_on})
+        var calDays = dateFns.eachDay(calStart, calEnd)
+        var calWeeks = ((days) => {
+            calWeeks = []
+            while (days.length) {
+                calWeeks.push(days.splice(0, 7));
+            }
+            return calWeeks
+        })(calDays)
+        var daysOfWeekMondayStart = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        var daysOfWeek = daysOfWeekMondayStart.slice(settings.week_starts_on-1).concat(daysOfWeekMondayStart.slice(0,settings.week_starts_on-1))
+        // I'm adding the splitting by pages here because even though it's not pure date data, it's relevant for the context
+        // There is a change that someone want a digital calendar and they won't care about page, but it's a nice "API" feature for those who do
+        var calPages = ((weeks) => {
+            calPages = [[]]
+            var i = 0, j=0
+            while (i < weeks.length) {
+                // if day0 > day6 (e.g. 28 > 4) then it's last week of month which is carried over to next month
+                var isLastWeek = dateFns.getDate(weeks[i][0]) > dateFns.getDate(weeks[i][6])
+                // if day0 is on 1st OR if last week but not first or last week then next page
+                if( (j!=0?dateFns.isFirstDayOfMonth(weeks[i][0]):false) || (isLastWeek && i!=0 && i!=weeks.length-1)) {
+                    j+=1
+                    calPages[j]=[]
+                }
+                calPages[j].push(weeks[i])
+                i++
+            }
+            return calPages
+        })(calWeeks)
+        // public values
+        this.weeks = calWeeks
+        this.daysOfWeek = daysOfWeek
+        this.pages = calPages
+    }
 }
-PDFWriter {
+function PDFWriter() {
     // Write the file
     var fs = require('fs'),
     PDFDocument = require('pdfkit'),
@@ -123,23 +132,32 @@ PDFWriter {
     })
     // might be worth separating the PDF generation too
     // from
-    SVGRenderer
+    //SVGRenderer...
 }
-class ArgsStuff {
-    // static for now, TODO make args
-    var start = {year: 2023, month: 1} // 1 = january, I usually start on December the year before, and end on the January the year after
-    var number_of_months = 12
-    //
-    //TODO gracefully catch errors
-    const myArgs = process.argv.slice(2);
-    var template = myArgs[0]
-    var weekStartsOn = myArgs[1] ; //1 = Monday, 7 = Sunday
+
+class Settings {
+    // node calendar.js start_year start_month number_of_months template week_starts_on
+    // node index-structure.js 2023 1 12 pocket 1
+    // eventually replace with API request parameters
+    constructor(myArgs) {
+        this.start = {year: myArgs[0], month: myArgs[1]} // 1 = january, I usually start on December the year before, and end on the January the year after
+        this.number_of_months = myArgs[2]
+        this.template = myArgs[3]
+        this.week_starts_on = myArgs[4] ; //1 = Monday, 7 = Sunday
+    }
 }
-main {
-    // get inputs from command line
-    const settings = new ArgsStuff()
-    myCalendar = Calendar(settings.start, settings.number_of_months, settings.weekStartsOn)
+
+function init() {
+    if (process.argv.length == 7) {
+        settings = new Settings(process.argv.slice(2))
+        console.log("Start on: ", settings.start, "\nNumber of months: ", settings.number_of_months, "\nTemplate: ", settings.template, "\nWeek starts on: ", settings.week_starts_on)
+    } else {
+        throw new Error('Arguments missing, please use start_year start_month number_of_months template week_starts_on');
+    }
+    myCalendar = new CalendarData(settings)
+    console.log(myCalendar.pages)
     // to call
-    PDFWriter...
+    
+    // PDFWriter...
 }
-main() // run
+init() // run
