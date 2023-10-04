80020be7548ec7876d53bc742ee948eb73166eba
Alex Brovkin <alex@webmovement.com>
2023-10-03 21:09:56 -0400

    take out more comments

--- index.js
+++ index.js
@@ -3,20 +3,6 @@ import CalendarData from './Classes/calendarData.js'
 import SVGCalendar from './Classes/svgCalendar.js'
 import OutputPDF from './Classes/outputPDF.js'
 
-// TODO major
-// - [ ] Making the SVGs... not sure how to structure right now
-// - [ ] Rewrite with factory functions instead of classes (better fit for JS)
-//       https://www.freecodecamp.org/news/class-vs-factory-function-exploring-the-way-forward-73258b6a8d15
-//       https://www.freecodecamp.org/news/removing-javascripts-this-keyword-makes-it-a-better-language-here-s-why-db28060cc086/
-// TODO minor
-// - [ ] Clean up variable names, e.g. camelCase vs underscores, etc.
-// - [ ] Catch errors eg template not found
-
-// QUESTIONS/FEEDBACK
-// - ( ) Case and naming suggestions needed for files, folders, variable names in different places...
-// - ( ) I'm not super keen on how settings for templates/paper and dates are together. I don't anticipate the library/API to be used to generate just calendar data by week, but it doesn't feel great to have it all together. 
-// - ( ) this. ... are looking messy in svgCalendar
-
 function init() {
     if (process.argv.length == 8) { // node file.js ... + [0...5]
         var settings = new Settings(process.argv.slice(2))
@@ -27,7 +13,7 @@ function init() {
                     "\nWeek starts on: ", settings.week_starts_on,          // myArgs[4]
                     "\nPaper: ", settings.paper                             // myArgs[5]
                     )
-    } else {
+    } else {    
         throw new Error('Arguments missing, please use start_year start_month number_of_months template week_starts_on paper_size \n for example: \n node index\ structure.js 2023 1 12 pocket 1 strip');
     }
     const myCalendarData = new CalendarData(settings)
